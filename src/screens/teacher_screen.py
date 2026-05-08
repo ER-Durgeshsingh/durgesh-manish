93047d248dbf8a02fdb9827a097ef6c2f30c9782
Durgeshwar Kumar Singh <durgeshsingh23032002@gmail.com>
2026-05-08 23:09:16 +0530

    Update teacher_screen.py

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -370,9 +370,9 @@ def teacher_screen_register():
     st.space()
 
     
-    teacher_username = st.text_input("Enter username", placeholder='ananyaroy')
+    teacher_username = st.text_input("Enter username", placeholder='durgeshsingh')
 
-    teacher_name = st.text_input("Enter name", placeholder='Ananya Roy')
+    teacher_name = st.text_input("Enter name", placeholder='Durgeshwar kumar Singh')
 
     teacher_pass = st.text_input("Enter password", type='password', placeholder="Enter password")
 
